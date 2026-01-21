67678191fa0767675083eeca6b003e69201a7774
Renan (Elfo) Aguiar <renan.aguiar@jccbr.com>
2026-01-21 11:19:24 -0300

    Added time of day in status bar

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -5,6 +5,8 @@ return {
     config = function()
       local statusline = require('mini.statusline')
       statusline.setup { use_icons = true }
+      -- Add time to the statusline
+      vim.o.statusline = vim.o.statusline .. ' %{strftime("%H:%M")}'
     end
   }
 }
