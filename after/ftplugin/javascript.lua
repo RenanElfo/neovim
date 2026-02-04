81404cef050e2af6ebd0ea4b3492f4d608207022
Renan (Elfo) Aguiar <renan.aguiar@jccbr.com>
2026-02-04 20:38:23 -0300

    Fix Javascript auto format/lint

--- after/ftplugin/javascript.lua
+++ after/ftplugin/javascript.lua
@@ -22,11 +22,9 @@ vim.api.nvim_create_augroup("AutoFormat", {})
 vim.api.nvim_create_autocmd(
     "BufWritePost",
     {
-        pattern = "*.py",
         group = "AutoFormat",
         callback = function()
             vim.cmd("silent !pnpm biome check --fix")            
-            -- vim.cmd("silent !uv run ruff format %")            
             vim.cmd("edit")
         end,
     }
